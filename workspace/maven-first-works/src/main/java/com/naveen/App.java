34ae02d382ebed85b58fd6868f6f725a6c58b386
Naveen <adith.naveen@gmail.com>
2021-10-25 16:20:23 +0530

    updated maven works

--- workspace/maven-first-works/src/main/java/com/naveen/App.java
+++ workspace/maven-first-works/src/main/java/com/naveen/App.java
@@ -1,7 +1,17 @@
 package com.naveen;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger; 
 public class App {
+	private static Logger logger = LoggerFactory.getLogger(App.class);
 	public static void main(String[] args) {
 		System.out.println("hello i'm in maven project...");
+		
+		logger.info("hi i'm from info");
+		logger.debug("hi i'm  debug");
+		logger.error("hi i'm from error");
+		
+		String name = "Sapient"; 
+		logger.debug("some values {}", name);
 	}
 }
