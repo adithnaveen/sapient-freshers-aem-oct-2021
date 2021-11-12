6dce2641aabc5ab68ba387187ce8e349c750086c
Naveen <adith.naveen@gmail.com>
2021-11-12 17:25:57 +0530

    added footer

--- workspace/user-mgmt/src/main/java/com/naveen/connection/GetConnection.java
+++ workspace/user-mgmt/src/main/java/com/naveen/connection/GetConnection.java
@@ -5,18 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class GetConnection {
-	private static  Connection connection; 
+	
+	private GetConnection(){}
+	
 	public static  Connection getMysql() {
+		Connection connection; 
 		// you are loading the connection 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection
 					("jdbc:mysql://localhost/sap_aem", "root", "kanchan@1");
-		
 			return connection;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		} 
 		return null; 
